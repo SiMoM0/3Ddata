69672d4519816e75eb07d5cbc9ad1419608a6062
SiMoM0 <simomosco99@gmail.com>
2023-05-28 13:41:20 +0200

    clean up svd code

--- Lab3/src/Registration.cpp
+++ Lab3/src/Registration.cpp
@@ -17,14 +17,17 @@ struct PointDistance
   ////////////////////////////////////////////////////////////////////////////////////////////////////
 
   PointDistance(Eigen::Vector3d  source, Eigen::Vector3d  target)
-    : source_point(std::move(source)), target_point(std::move(target)) {}
+    : source(source), target(target) {}
 
   template <typename T>
   bool operator()(const T* const transformation, T* residual) const {
+      // extract source data
+      Eigen::Matrix<T, 3, 1> source_point;
+      source_point << T(source[0]), T(source[1]), T(source[2]);
       // Extract the translation parameters
-      const T tx = transformation[3];
-      const T ty = transformation[4];
-      const T tz = transformation[5];
+      T tx = transformation[3];
+      T ty = transformation[4];
+      T tz = transformation[5];
 
       // Apply the rotation and translation to the source point
       Eigen::Matrix<T, 3, 1> rotated_point;
@@ -34,19 +37,19 @@ struct PointDistance
       rotated_point[2] += tz;
 
       // Compute the residual as the difference between the transformed source point and the target point
-      residual[0] = rotated_point.x() - target_point.x();
-      residual[1] = rotated_point.y() - target_point.y();
-      residual[2] = rotated_point.z() - target_point.z();
+      residual[0] = rotated_point.x() - target.x();
+      residual[1] = rotated_point.y() - target.y();
+      residual[2] = rotated_point.z() - target.z();
 
       return true;
   }
 
-  /*static ceres::CostFunction* Create(Eigen::Vector3d& source, Eigen::Vector3d& target) {
-      return new ceres::AutoDiffCostFunction<PointDistance, 3, 6>(new);
-  }*/
+  static ceres::CostFunction* Create(Eigen::Vector3d& source, Eigen::Vector3d& target) {
+      return new ceres::AutoDiffCostFunction<PointDistance, 3, 6>(new PointDistance(source, target));
+  }
 
-  Eigen::Vector3d source_point;
-  Eigen::Vector3d target_point;
+  Eigen::Vector3d source;
+  Eigen::Vector3d target;
 };
 
 
@@ -100,17 +103,21 @@ void Registration::execute_icp_registration(double threshold, int max_iteration,
   //Remember to update transformation_ class variable, you can use source_for_icp_ to store transformed 3d points.
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  // rmse of previous iteration
   double prev_rmse = 0.0;
 
   for(int i=0; i<max_iteration; ++i) {
       std::printf("Iteration [%d]\n", i);
       std::tuple<std::vector<size_t>, std::vector<size_t>, double> tuple = find_closest_point(threshold);
 
+      // extract rmse of current iteration
       double rmse = std::get<2>(tuple);
 
+      // check convergence
       if(std::abs(rmse - prev_rmse) < relative_rmse)
           break;
 
+      // update
       prev_rmse = rmse;
 
       Eigen::Matrix4d prev_transformation = get_transformation();
@@ -147,19 +154,24 @@ std::tuple<std::vector<size_t>, std::vector<size_t>, double> Registration::find_
   Eigen::Vector3d source_point;
   double rmse;
 
+  // compute KD Tree for target point cloud
   open3d::geometry::KDTreeFlann target_kd_tree(target_);
   std::vector<int> idx(1);
   std::vector<double> dist2(1);
 
+  // clone source point cloud
   open3d::geometry::PointCloud source_clone = source_for_icp_;
 
-  size_t source_points = source_clone.points_.size();
+  // number of points in source point cloud
+  int num_points = source_clone.points_.size();
 
-  for(size_t i=0; i < source_points; ++i) {
+  // find closest for each point in source point cloud
+  for(size_t i=0; i<num_points; ++i) {
       source_point = source_clone.points_[i];
       target_kd_tree.SearchKNN(source_point, 1, idx, dist2);
 
-      if(sqrt(dist2[0]) < threshold) {
+      // check if distance is lower than the threshold
+      if(sqrt(dist2[0]) <= threshold) {
           source_indices.push_back(i);
           target_indices.push_back(idx[0]);
           rmse = rmse * i/(i+1) + dist2[0]/(i+1);
@@ -168,7 +180,7 @@ std::tuple<std::vector<size_t>, std::vector<size_t>, double> Registration::find_
 
   rmse = sqrt(rmse);
 
-  std::printf("RMSE in find_closest_point = %f\n", rmse);
+  // std::printf("RMSE in find_closest_point = %f\n", rmse);
 
   return {source_indices, target_indices, rmse};
 }
@@ -182,66 +194,46 @@ Eigen::Matrix4d Registration::get_svd_icp_transformation(std::vector<size_t> sou
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity(4,4);
 
+  // clone source point cloud
   open3d::geometry::PointCloud source_clone = source_for_icp_;
 
-  // TODO check if exists a getCentroid method
-  Eigen::Vector3d source_centroid {0, 0, 0};
-  Eigen::Vector3d target_centroid {0, 0, 0};
-
-  size_t source_points = source_clone.points_.size();
-  size_t target_points = target_.points_.size();
-
-  for(size_t i=0; i< source_points; ++i) {
-      source_centroid += source_clone.points_[i];
-  }
-  for(size_t i=0; i< target_points; ++i) {
-      target_centroid += target_.points_[i];
-  }
-
-  source_centroid /= (double) source_points;
-  target_centroid /= (double) target_points;
-
-  // auto mean_covariance = source_clone.ComputeMeanAndCovariance();
-  // auto mean_covariance2 = target_.ComputeMeanAndCovariance();
-
-  // source_centroid = std::get<0>(mean_covariance);
-  // target_centroid = std::get<0>(mean_covariance2);
-
-  Eigen::MatrixXd spoints (3, source_indices.size());
-  Eigen::MatrixXd tpoints (3, target_indices.size());
+  // initialize centroids for both point clouds
+  Eigen::Vector3d source_centroid = std::get<0>(source_clone.ComputeMeanAndCovariance());
+  Eigen::Vector3d target_centroid = std::get<0>(target_.ComputeMeanAndCovariance());
 
   Eigen::Matrix3d W;
 
-  // subtract centroids to point clouds points
+  // compute matrix W
   for(int i=0; i<source_indices.size(); ++i) {
+      // get normalized points
       Eigen::Vector3d d = source_clone.points_[source_indices[i]] - source_centroid;
       Eigen::Vector3d m = target_.points_[target_indices[i]] - target_centroid;
 
-      Eigen::Matrix3d prod = m * d.transpose();
-      W += prod;
+      // update W
+      W += m * d.transpose();
   }
 
-  // Eigen::Matrix3d m = spoints * tpoints.transpose();
-
+  // perform SVD
   Eigen::JacobiSVD<Eigen::MatrixXd> svd(W, Eigen::ComputeFullU | Eigen::ComputeFullV);
 
-  // Compute the rotation matrix (U * V^T) and translation vector
+  // Compute the rotation matrix
   Eigen::Matrix3d R = svd.matrixU() * svd.matrixV().transpose();
 
   // Handle the special reflection case
-  // det(U) * det(V) == -1
   if (svd.matrixU().determinant() * svd.matrixV().determinant() == -1) {
-      std::printf("Reflection case\n");
+      // define diagonal matrix with (1, 1, -1)
       Eigen::DiagonalMatrix<double, 3> diagonal_matrix;
       diagonal_matrix.diagonal() << 1.0, 1.0, -1.0;
-      // Multiply by diagonal matrix of (1, 1, -1)
+      // Update R
       R = svd.matrixU() * diagonal_matrix * svd.matrixV().transpose();
   }
 
+  // compute translation vector
   Eigen::Vector3d t = target_centroid - R * source_centroid;
 
   // std::cout << R << " " << t << std::endl;
 
+  // set transformation matrix with R and t
   transformation.block<3, 3>(0, 0) = R;
   transformation.block<3, 1>(0, 3) = t;
 
@@ -259,18 +251,33 @@ Eigen::Matrix4d Registration::get_lm_icp_registration(std::vector<size_t> source
   //use source_indices and target_indices to extract point to compute the matrix to be decomposed.
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity(4,4);
+  ceres::Problem problem;
+  ceres::Solver::Summary summary;
   ceres::Solver::Options options;
   options.minimizer_progress_to_stdout = false;
   options.num_threads = 4;
   options.max_num_iterations = 100;
 
+  // clone source point cloud
+  open3d::geometry::PointCloud source_clone = source_for_icp_;
+
   std::vector<double> transformation_arr(6, 0.0);
   int num_points = source_indices.size();
+
+  Eigen::Vector3d source_point, target_point;
+
   // For each point....
   for( int i = 0; i < num_points; i++ )
   {
-    
+    source_point = source_clone.points_[source_indices[i]];
+    target_point = target_.points_[target_indices[i]];
+
+    ceres::CostFunction* cost_function = PointDistance::Create(source_point, target_point);
+    problem.AddResidualBlock(cost_function, NULL, transformation_arr.data());
   }
+
+  // solve problem
+  Solve(options, &problem, &summary);
 
   return transformation;
 }
